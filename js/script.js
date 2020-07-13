37f771f407aaf9c8b8e715c819b01c758e26519e
Charles Eubanks <charles.patrick.eubanks@gmail.com>
2020-07-13 13:17:34 -0400

    updated js file with initial vars

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// API URL w my key: https://www.rijksmuseum.nl/api/nl/collection?key=fk8HjIgd&involvedMaker=Rembrandt+van+Rijn
+
+// Constants
+const baseURL = `https://www.rijksmuseum.nl/api/nl/collection?key=${}&involvedMaker=Rembrandt+van+Rijn`;
+
+
+// App's State Variables
+let artWork, input, artDetails
+
+
+// Cached Element References
+const $ulEl = $
+
+
+// Event Listeners
+
+
+// Functions
+
+// fetch data from Rijksmuseum
+
+function getArt() {
+    const url = baseURL;
+    $.ajax(url)
+        .then(function(data) {
+
+        })
+
+}
