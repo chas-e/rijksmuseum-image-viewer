0ae118c6914f02f4d0e4fe94aa7f856e79f90d15
Charles Eubanks <charles.patrick.eubanks@gmail.com>
2020-07-17 16:59:17 -0400

    trying to fix ajax req

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ function getArt() {
 
     input = $usrInputEl.val();
 
-    $.ajax(`https://project1-629.herokuapp.com/chas?search=${input}`)
+    $.ajax(`https://project1-629.herokuapp.com/chas?title=${input}`)
         .then(function(data) {
                 artWork = data;
                 checkData();
