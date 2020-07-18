0bfd8543cdd0e783f989f1907b4b6d48430a67fa
Charles Eubanks <charles.patrick.eubanks@gmail.com>
2020-07-18 10:56:52 -0400

    added JQuery IIFE

--- js/script.js
+++ js/script.js
@@ -1,90 +1,93 @@
-// Constants
-const $usrInputEl = $("#usr-input");
+// jQuery IIFE
+$(function() {
+    // Constants
+    const $usrInputEl = $("#usr-input");
 
 
-// App's State Variables
-let artWork, input;
+    // App's State Variables
+    let artWork, input;
 
 
-// Cached Element References
-const $tableEl = $("#content");
-const $artTableEl = $("#art-table");
-const $imgEl = $("#image-content");
-const $subBtn = $("#submit");
-const $section = $("#small-container");
+    // Cached Element References
+    const $tableEl = $("#content");
+    const $artTableEl = $("#art-table");
+    const $imgEl = $("#image-content");
+    const $subBtn = $("#submit");
+    const $section = $("#small-container");
 
 
 
-// Event Listeners
-$subBtn.on('click', handleClick);
+    // Event Listeners
+    $subBtn.on('click', handleClick);
 
-// Functions
-// hide empty table and header
-function hideEls() {
-    $section.hide();
-}
-
-// show table and header when data is retrieved and rendered
-function showEls() {
-    $section.show();
-}
-
-// clear out input area after running search
-function clearInput() {
-    $usrInputEl.val(null);
-}
-
-// click handler for submit button
-function handleClick(event) {
-    getArt();
-    clearInput();
-}
-
-// fetch data from Rijksmuseum
-function getArt() {
-
-    input = $usrInputEl.val();
-
-    $.ajax(`https://project1-629.herokuapp.com/chas?search=${input}`)
-        .then(function(data) {
-                artWork = data;
-                checkData();
-            },
-
-            function(error) {
-                alert(error.responseJSON.message);
-            });
-}
-
-// check to see if any artObjects are present after AJAX promise
-function checkData() {
-    if (artWork.count === 0) {
-        renderError();
-    } else {
-        render();
+    // Functions
+    // hide empty table and header
+    function hideEls() {
+        $section.hide();
     }
-}
 
-// Package Data elements into html to ender in the DOM
-function generateHTML() {
-    return artWork.artObjects.map(function(art) {
-        return `<tr>
+    // show table and header when data is retrieved and rendered
+    function showEls() {
+        $section.show();
+    }
+
+    // clear out input area after running search
+    function clearInput() {
+        $usrInputEl.val(null);
+    }
+
+    // click handler for submit button
+    function handleClick(event) {
+        getArt();
+        clearInput();
+    }
+
+    // fetch data from Rijksmuseum
+    function getArt() {
+
+        input = $usrInputEl.val();
+
+        $.ajax(`https://project1-629.herokuapp.com/chas?search=${input}`)
+            .then(function(data) {
+                    artWork = data;
+                    checkData();
+                },
+
+                function(error) {
+                    alert(error.responseJSON.message);
+                });
+    }
+
+    // check to see if any artObjects are present after AJAX promise
+    function checkData() {
+        if (artWork.count === 0) {
+            renderError();
+        } else {
+            render();
+        }
+    }
+
+    // Package Data elements into html to ender in the DOM
+    function generateHTML() {
+        return artWork.artObjects.map(function(art) {
+            return `<tr>
             <td>${art.title}</td>
             <td>${art.principalOrFirstMaker}</td>
             <td><img class="responsive-img" src="${art.webImage ? art.webImage.url :"#"}"</td>
         </tr>`;
-    });
-}
+        });
+    }
 
-// render the html string into the DOM
-function render() {
-    showEls();
-    const html = generateHTML().join('');
-    $tableEl.html(html);
-}
+    // render the html string into the DOM
+    function render() {
+        showEls();
+        const html = generateHTML().join('');
+        $tableEl.html(html);
+    }
 
-// render an error message via a toast using materialize functionality - also run hideEls()
-function renderError() {
-    hideEls();
-    M.toast({ html: "Sorry nothing matched that search, please try again." });
-}
+    // render an error message via a toast using materialize functionality - also run hideEls()
+    function renderError() {
+        hideEls();
+        M.toast({ html: "Sorry nothing matched that search, please try again." });
+    }
+});
